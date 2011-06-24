6ad824dcd7a90d06ac4d037d95e74c01afbb1e1c
Victoria <victoria@ringo.config>
2011-06-24 20:35:03 +0100

    Don't show comments

--- page.php
+++ page.php
@@ -64,10 +64,10 @@
        		if (THEMATIC_COMPATIBLE_COMMENT_HANDLING) {
 				if ( get_post_custom_values('comments') ) {
 					// Add a key/value of "comments" to enable comments on pages!
-					thematic_comments_template();
+					//thematic_comments_template();
 				}
 			} else {
-				thematic_comments_template();
+				//thematic_comments_template();
 			}
 	        
 	        // calling the widget area 'page-bottom'
